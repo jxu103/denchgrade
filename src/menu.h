7621b82f76f4962e5a1df2a91105bfbccc38e656
Jingyuan Xu <jxu103@ucr.edu>
2018-12-18 19:00:28 -0800

    moved login to menu class

---
+++ src/menu.h
@@ -0,0 +1,64 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "account.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Menu {
+    public:
+        Menu(){}
+
+        void login() {
+            Account* account = new Account();	
+	        bool userLogin = 0;
+	        bool exitCall = 0;
+
+            while(exitCall == 0) {
+                cout << "Please choose an option from the menu" << endl;
+                cout << "1. Login" << endl;
+                cout << "2. Create Account" << endl;
+                cout << "3. Exit" << endl;
+
+                //Prompting User
+                string userInput;
+                cin >> userInput;
+                
+
+                if(userInput == "1") {
+                    //calls login function	
+                    bool status = account->login();
+                    if(!status) {
+                        // Should not end program here, loop to menu
+                        cout << "The username or password you've entered is incorrect" << endl;
+                    }
+                    else {
+                        // Unimplemented
+                        cout << "Correct login!" << endl;
+                        cout << "Welcome back: " << account->getUsername() << "!" << endl;
+                        exitCall = 1;
+                    }
+                    
+                }
+                else if(userInput == "2") {
+                    account->signup();
+                    cout << "Signup successful!" << endl;
+                    // Unimplemented
+                    exitCall = 0;
+                }
+                else if(userInput == "3") {
+                    cout <<"Thanking for using Dench Grading System" << endl;
+                    exitCall = 1;
+                }
+                else {
+                    cout <<"Invalid selection" << endl;
+                }
+            }
+        }
+
+
+};
+
+#endif
--- src/run.cpp
+++ src/run.cpp
@@ -1,57 +1,17 @@
 #include "account.h"
+#include "menu.h"
 
 using namespace std;
 
 int main() {
-	Account* account = new Account();	
-	bool userLogin = 0;
-	bool exitCall = 0;
-
 	system("clear");
 
 	cout << "Welcome to Dench Grading System..." << endl;
-	
-	while(exitCall == 0) {
-		cout << "Please choose an option from the menu" << endl;
-		cout << "1. Login" << endl;
-		cout << "2. Create Account" << endl;
-		cout << "3. Exit" << endl;
 
-		//Prompting User
-		string userInput;
-		cin >> userInput;
-		
+	//program verified account login
+	Menu* menu = new Menu();
+	menu->login();
 
-		if(userInput == "1") {
-			//calls login function	
-			bool status = account->login();
-			if(!status) {
-				// Should not end program here, loop to menu
-				cout << "The username or password you've entered is incorrect" << endl;
-			}
-			else {
-				// Unimplemented
-				cout << "Correct login!" << endl;
-				exitCall = 1;
-			}
-			
-		}
-		else if(userInput == "2") {
-			account->signup();
-			cout << "Signup successful!" << endl;
-			// Unimplemented
-			exitCall = 0;
-		}
-		else if(userInput == "3") {
-			cout <<"Thanking for using Dench Grading System" << endl;
-			exitCall = 1;
-		}
-		else {
-			cout <<"Invalid selection" << endl;
-		}
-	}
-
-	cout << "Welcome back: " << account->getUsername() << "!" << endl;
 
 return 0;
 }
